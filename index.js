6bedc663e37ae7d3872b6725ee09145cfc3652fd
Cain Tapp <cainlucastapp@gmail.com>
2025-08-15 14:19:55 -0700

    Added Attendee Functions

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 //Write your code here
 
+const attendee = {attendeeId:"T001",name:"Alice Smith",event:"JavaScript Conference",ticketType:"VIP",ticketPrice:150.00}
 
+//Log Name
+function logAttendeeName(attendee){
+  console.log(attendee.name);
+}
+
+// Log Ticket Price
+function logTicketPrice(){
+  console.log(attendee.ticketPrice);
+}
+
+//Update Ticket Type
+function updateTicketType(attendee,ticketType){
+  attendee.ticketType = ticketType;
+}
+
+//Update Ticket Price
+function updateTicketPrice(attendee,ticketPrice){
+  attendee.ticketPrice = ticketPrice;
+}
+
+//Remove Event Property
+function removeEventProperty(attendee){
+  delete attendee.event;
+}
+
+//Check In Property
+function addCheckedInProperty(attendee){
+  attendee.checkedIn = true;
+}
 
 //Needed for the tests to work. Don't modify
 module.exports = {
